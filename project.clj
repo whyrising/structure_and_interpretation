e189e36b8cf14c221940c2aff83f63e368a67742
Yahya <yahyatinani@gmail.com>
2020-01-17 16:02:42 +0100

    add clojure project files

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject structure_and_interpretation "0.1.0-SNAPSHOT"
+  :description "Practicing functional style"
+  :url "https://github.com/whyrising"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :repl-options {:init-ns structure-and-interpretation.core})
